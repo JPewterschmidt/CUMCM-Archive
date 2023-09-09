8d8af6d45fcfc3e46aec267ca9d73fcd270a1108
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2023-09-10 06:44:53 +0800

    question no.1 functions complete!!!

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from builtins import max
 from threading import local
 
+from functools import lru_cache
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -49,7 +51,9 @@ def split_dataframe(df, num_splits, workers, process_func):
     results = [future.result() for future in futures]
 
     return results
+
 #%% some function about the sun
+@lru_cache(maxsize=2000)
 def solar_zenith_angle(latitude_deg, declination, hour_angle):
     """
     Calculate the solar zenith angle.(太阳高度角)
@@ -92,6 +96,7 @@ def hour_minute_to_decimal_time(hour, minute):
     decimal_time = hour + minute / 60.0
     return decimal_time
 
+@lru_cache(maxsize=2000)
 def solar_declination(day_of_year):
     """
     Calculate the solar declination angle.(赤纬角)
@@ -104,6 +109,7 @@ def solar_declination(day_of_year):
           * math.sin(math.radians(360 * (284 + n) / 365)))
     return delta
 
+@lru_cache(maxsize=2000)
 def hour_angle(local_time):
     """
     Calculate the hour angle.(时角)
@@ -115,6 +121,7 @@ def hour_angle(local_time):
     return H
 
 
+@lru_cache(maxsize=2000)
 def day_of_year_from_date(year, month, day):
     """
     Calculate the day of the year (DOY) from a given year, month, and day.
@@ -132,6 +139,7 @@ def day_of_year_from_date(year, month, day):
         return -1  # Invalid date
 
 
+@lru_cache(maxsize=2000)
 def solar_azimuth_angle(latitude_deg, zenith_angle):
     """
     Calculate the azimuth angle.(方位角)
@@ -206,6 +214,9 @@ df['vector_h2r_x_unit'] = df.vector_h2r_x / df.vector_h2r_len
 df['vector_h2r_y_unit'] = df.vector_h2r_y / df.vector_h2r_len
 df['vector_h2r_z_unit'] = df.vector_h2r_z / df.vector_h2r_len
 
+#atmospheric attenuation
+df['atmospheric_attenuation'] = 0.99321 - 0.0001176 * df.vector_h2r_len + 1.97e-8 * df.vector_h2r_len**2
+
 #%% vector from helio to sun
 def vector_h2s_x(day_of_year, local_time):
     decli = solar_declination(day_of_year)
@@ -227,6 +238,7 @@ def vector_h2s_z(day_of_year, local_time):
     zenith = solar_zenith_angle(global_latitude, decli, hourang)
     return np.sin(zenith)
 
+@lru_cache(maxsize=1000)
 def vector_of_heliostat_to_sun_unit(day_of_year, local_time):
     h2s_x = vector_h2s_x(day_of_year, local_time)
     h2s_y = vector_h2s_y(day_of_year, local_time)
@@ -235,7 +247,8 @@ def vector_of_heliostat_to_sun_unit(day_of_year, local_time):
     norm = (h2s_x**2 + h2s_y**2 + h2s_z**2)**0.5
     return np.array([h2s_x / norm, h2s_y / norm, h2s_z / norm]).T
 
-def cosine_effiency(day_of_year, local_time):
+@lru_cache(maxsize=2000)
+def cosine_effiency_of_each_heliostat(day_of_year, local_time):
     vec_h2s_unit = vector_of_heliostat_to_sun_unit(day_of_year, local_time)
     result = pd.DataFrame()
     result['cosine_eff'] = value_cos2b_to_value_cosb(
@@ -246,6 +259,7 @@ def cosine_effiency(day_of_year, local_time):
     )
     return result['cosine_eff']
 
+@lru_cache(maxsize=2000)
 def direct_normal_irradiance(day_of_year, local_time, altitude, latitude):
     """
     :param day_of_year:
@@ -272,7 +286,7 @@ def full_reflective_power(day_of_year, local_time, heliostat_area):
     :param heliostat_area:
     :return: the power of full reflective (kW)
     """
-    cos_eff = cosine_effiency(day_of_year, local_time)
+    cos_eff = cosine_effiency_of_each_heliostat(day_of_year, local_time)
     return (cos_eff
             * heliostat_area
             * direct_normal_irradiance(
@@ -295,7 +309,7 @@ for month in range(1, 12 + 1):
 
     total_cos_eff = 0
     for t in local_time:
-        total_cos_eff += cosine_effiency(doy, t).mean()
+        total_cos_eff += cosine_effiency_of_each_heliostat(doy, t).mean()
 
     avg_cos_eff = total_cos_eff / len(local_time)
     results.append(avg_cos_eff)
@@ -312,6 +326,7 @@ def normalize_vector(row):
     else:
         return row  # Avoid division by zero for zero vectors
 
+@lru_cache(maxsize=1000)
 def normal_vector_of_heliostat(doy, local_time):
     """
     :param doy: day of year
@@ -327,6 +342,7 @@ def normal_vector_of_heliostat(doy, local_time):
     normal.rename(columns=new_column_names, inplace=True)
     return normal
 
+@lru_cache(maxsize=1000)
 #%% Calculate the heliostat azimuth angle
 def heliostat_azimuth_angle(doy, local_time):
     """
@@ -339,6 +355,7 @@ def heliostat_azimuth_angle(doy, local_time):
     return azimuth
 
 #%% Calculate the heliostat tilt angle
+@lru_cache(maxsize=1000)
 def heliostat_tilt_angle(doy, local_time):
     """
     :param doy: day of year
@@ -349,6 +366,7 @@ def heliostat_tilt_angle(doy, local_time):
     tilt = np.arctan(np.sqrt(normal.normal_x_unit**2 + normal.normal_y_unit**2) / normal.normal_z_unit)
     return tilt
 
+@lru_cache(maxsize=1000)
 def heliostat_steering_angle(doy, local_time):
     """
     :param doy: day of year
@@ -371,6 +389,7 @@ def down_left_corner_local_coordinate(central_x, central_y, length, width):
 def down_right_corner_local_coordinate(central_x, central_y, length, width):
     return central_x + 0.5 * length, central_y - 0.5 * width
 
+@lru_cache(maxsize=2000)
 def each_global_coordinate(local_x, local_y, doy, local_time, width, length):
     """
     :param doy: day of year
@@ -438,6 +457,7 @@ def down_right_corner_global_coordinate(doy, local_time, width, length):
     return each_global_coordinate(local_x, local_y, doy, local_time, width, length)
 
 #%% function of the cooridinate of the projection of each heliostat on the ground
+@lru_cache(maxsize=2000)
 def heliostat_corner_projection_on_ground(doy, local_time, width, length, corner_global_coordinate_func):
     zenith = solar_zenith_angle(global_latitude, solar_declination(doy), hour_angle(local_time))
 
@@ -476,13 +496,6 @@ def polygon_of_heliostat_on_the_ground(doy, local_time, width, length):
     projection = projection_of_heliostat_on_the_ground(doy, local_time, width, length)
     return projection.apply(lambda x: Polygon(x), axis=1)
 
-#%% the whole area of the heliostats' projection on the ground
-def union_heliostat_projection_on_the_ground(polygons):
-    whole_proj = Polygon()
-    for p in polygons:
-        whole_proj = p.union(whole_proj)
-    return whole_proj
-
 #%% overlapping parts of projections of heliostats on the ground
 def overlappings_of_projection(polygons):
     overlappings = []
@@ -494,9 +507,10 @@ def overlappings_of_projection(polygons):
     return unions
 
 #%% shadowing effeciency
-def shadowing_efficiency(doy, local_time, width, length):
+@lru_cache(maxsize=2000)
+def shadowing_efficiency_of_the_whole_field(doy, local_time, width, length):
     zenith = solar_zenith_angle(global_latitude, solar_declination(doy), hour_angle(local_time))
-    if zenith == 0: return 0
+    if zenith <= 0: return 0
 
     ps = polygon_of_heliostat_on_the_ground(doy, local_time, width, length)
 
@@ -523,7 +537,7 @@ def shadowing_efficiency(doy, local_time, width, length):
 #print(shadowing_efficiency(80, 12, 6, 6))
 
 #%% solar escaped height
-def height_solar_escaped_ratio(doy, local_time, width, length):
+def height_solar_escaped_ratio_of_each_heliostat(doy, local_time, width, length):
     # height of the lower side of heliostat
     local_x, local_y = down_left_corner_local_coordinate(0, 0, length, width)
     x, y, height = each_global_coordinate(local_x, local_y, doy, local_time, width, length)
@@ -535,15 +549,162 @@ def height_solar_escaped_ratio(doy, local_time, width, length):
     escaped_height = 80 - end_height
     ratio = escaped_height / solar_height
     ratio[ratio < 0] = 0
+    ratio[ratio > 1] = 1
     return ratio
 
 #print(height_solar_escaped_ratio(80, 12, 6, 6))
 
 #%% the turncate effiency
-def turncate_efficiency(doy, local_time, width, length):
-    return ((
-        1 - height_solar_escaped_ratio(doy, local_time, width, length))
-            / shadowing_efficiency(doy, local_time, width, length)
-    ).mean()
+@lru_cache(maxsize=2000)
+def turncate_efficiency_of_each_heliostat(doy, local_time, width, length):
+    return (
+        (1 - height_solar_escaped_ratio_of_each_heliostat(doy, local_time, width, length))
+        / shadowing_efficiency_of_the_whole_field(doy, local_time, width, length)
+    )
 
-print(turncate_efficiency(80, 17.8, 6, 6))
+#%% the optical effiency of heliostats
+@lru_cache(maxsize=1000)
+def optical_efficiency_of_each_heliostat(doy, local_time, width, length, mirror_reflectivity=0.92):
+    return (turncate_efficiency_of_each_heliostat(doy, local_time, width, length)
+            * shadowing_efficiency_of_the_whole_field(doy, local_time, width, length)
+            * cosine_effiency_of_each_heliostat(doy, local_time)
+            * df.atmospheric_attenuation
+            * mirror_reflectivity)
+
+#%% the power of each heliostat
+@lru_cache(maxsize=1000)
+def power_of_each_heliostat(doy, local_time, width, length, mirror_reflectivity=0.92):
+    return (optical_efficiency_of_each_heliostat(doy, local_time, width, length, mirror_reflectivity)
+            * direct_normal_irradiance(doy, local_time, global_altitude, global_latitude)
+            * width * length)
+
+#%% the power of the whole field
+def power_of_the_whole_field(doy, local_time, width, length, mirror_reflectivity=0.92):
+    return power_of_each_heliostat(doy, local_time, width, length, mirror_reflectivity).sum()
+
+#%% the power of the whole field per unit area
+def power_of_the_whole_field_per_unit_area(doy, local_time, width, length, field_area, mirror_reflectivity=0.92):
+    return power_of_each_heliostat(doy, local_time, width, length, mirror_reflectivity).sum() / field_area
+
+#%% some useful functions of heliostats
+def total_area_of_heliostats(width, length):
+    return width * length * len(df)
+
+def number_of_heliostats():
+    return len(df)
+
+#%% question 1 solution
+
+# dimension of the heliostats
+width = 6
+length = 6
+
+# local_time
+local_time = []
+local_time.append(9)
+local_time.append(10.5)
+local_time.append(12)
+local_time.append(13.5)
+local_time.append(15)
+
+# doy
+doy = []
+for month in range(1, 13):
+    doy.append(day_of_year_from_date(2023, month, 21))
+
+#with ProcessPoolExecutor(12) as executor:
+# day average of each properities of the field
+def cosine_eff_func():
+    # cosine eff
+    cos_eff = []
+    for d in doy:
+        cos_eff_per_day = []
+        for t in local_time:
+            cos_eff_per_day.append(cosine_effiency_of_each_heliostat(d, t).mean())
+        cos_eff.append(np.average(cos_eff_per_day, axis=0))
+        print("cos eff ", d, "day OK")
+    return cos_eff
+
+def shadowing_eff_func():
+    # shadowing eff
+    shadow_eff = []
+    for d in doy:
+        shadow_eff_per_day = []
+        for t in local_time:
+            shadow_eff_per_day.append(shadowing_efficiency_of_the_whole_field(d, t, width, length))
+        shadow_eff.append(np.average(shadow_eff_per_day, axis=0))
+        print("shadow eff ", d, "day OK")
+    return shadow_eff
+
+def turncate_eff_func():
+    # turncate eff
+    turncate_eff = []
+    for d in doy:
+        turncate_eff_per_day = []
+        for t in local_time:
+            per_day_val = turncate_efficiency_of_each_heliostat(d, t, width, length).mean()
+            turncate_eff_per_day.append(per_day_val)
+        turncate_eff.append(np.average(turncate_eff_per_day, axis=0))
+        print("turncate eff ", d, "ok")
+    return turncate_eff
+
+def optical_eff_func():
+    # optical eff
+    optical_eff = []
+    for d in doy:
+        optical_eff_per_day = []
+        for t in local_time:
+            optical_eff_per_day.append(optical_efficiency_of_each_heliostat(d, t, width, length).mean())
+        optical_eff.append(np.average(optical_eff_per_day, axis=0))
+        print("opti eff ", d, "day OK")
+    return optical_eff
+
+def power_per_unit_func():
+    # power of each heliostat per unit
+    power_per_unit = []
+    for d in doy:
+        power_per_unit_per_day = []
+        for t in local_time:
+            power_per_unit_per_day.append(power_of_each_heliostat(d, t, width, length).mean())
+        power_per_unit.append(np.average(power_per_unit_per_day, axis=0))
+        print("power per unit ", d, "day OK")
+    return power_per_unit
+
+with ProcessPoolExecutor(max_workers=12) as executor:
+    cos_eff = executor.submit(cosine_eff_func)
+    shadow_eff = executor.submit(shadowing_eff_func)
+    turncate_eff = executor.submit(turncate_eff_func)
+    optical_eff = executor.submit(optical_eff_func)
+    power_per_unit = executor.submit(power_per_unit_func)
+
+# arrange those stuff into a DataFrame
+df_q1 = pd.DataFrame()
+df_q1['optical_eff'] = optical_eff.result()
+print("opt ok")
+df_q1['cos_eff'] = cos_eff.result()
+print("cos ok")
+df_q1['shadow_eff'] = shadow_eff.result()
+print("sha ok")
+df_q1['turncate_eff'] = turncate_eff.result()
+print("turn ok")
+df_q1['power_per_unit'] = power_per_unit.result()
+print("power ok")
+
+#%% draw a hot map while color scheme is blue by sns
+# and I want the figure a little bigger which could give me a good print quality
+import seaborn as sns
+df_q1_2 = df_q1.drop('power_per_unit', axis=1)
+sns.heatmap(df_q1_2, cmap='Blues', annot=True, fmt='.3f', linewidths=.5, annot_kws={"size": 10})
+plt.yticks(rotation=0)
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.savefig('heliostat_properties.png', dpi=300, bbox_inches='tight')
+#plt.show()
+plt.close()
+
+#%% save the df_q1 into a csv file
+df_q1['month'] = range(1, 13)
+df_q1.to_csv('heliostat_properties.csv')
+
+#%% year average of each properities of the field
+df_q1_year = df_q1.mean(axis=0)
